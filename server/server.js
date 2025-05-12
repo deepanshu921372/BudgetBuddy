50eeb066c1c5096fb39995ed5da682e4509c7e7c
deepanshu921372 <sharmadeepanshu1005@gmail.com>
2025-05-12 13:04:46 +0530

    update

--- server/server.js
+++ server/server.js
@@ -53,11 +53,7 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
